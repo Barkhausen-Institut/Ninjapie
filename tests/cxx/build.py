d815eb5326a01da182ec1e3f7bc27bb121bed215
Nils Asmussen <nils@os.inf.tu-dresden.de>
2023-03-18 19:41:37 +0100

    Added simple C++ test.

---
+++ tests/cxx/build.py
@@ -0,0 +1,9 @@
+from ninjapie import Generator, Env
+
+gen = Generator()
+env = Env()
+
+env['CXXFLAGS'] += ['-Wall', '-Wextra']
+env.cxx_exe(gen, out = 'hello', ins = ['hello.cc'])
+
+gen.write_to_file(env)
