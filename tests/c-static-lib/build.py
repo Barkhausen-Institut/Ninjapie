7b6d14c5e2442ae9e0aa1c1fae5487b8b5a62283
Nils Asmussen <nils@os.inf.tu-dresden.de>
2023-03-19 07:47:57 +0100

    Added static library test.

---
+++ tests/c-static-lib/build.py
@@ -0,0 +1,11 @@
+from ninjapie import Generator, Env
+
+gen = Generator()
+env = Env()
+
+env['CFLAGS'] += ['-Wall', '-Wextra', '-pedantic']
+
+env.sub_build(gen, 'foo')
+env.c_exe(gen, out = 'hello', ins = ['hello.c'], libs = ['foo'])
+
+gen.write_to_file(env)
---
+++ tests/c-static-lib/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "foo/foo.h"
+
+int main() {
+    printf("Hello %d!\n", foo(10, 14));
+    return 0;
+}
