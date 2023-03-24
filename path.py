c6b935c31eb0730f3b241cef328daaeef06659f6
Nils Asmussen <nils@os.inf.tu-dresden.de>
2023-03-24 19:44:42 +0100

    Added documentation to SourcePath and BuildPath.

--- path.py
+++ path.py
@@ -2,10 +2,45 @@ import os
 
 
 class SourcePath(str):
+    """
+    A path for source files
+
+    This class is used to refer to source files. These are specified relative to the current
+    directory in the given environment (`Env.cur_dir`).
+
+    `SourcePath` implements `__str__` and `__repr__` and can thus be used as a string (the contained
+    path).
+    """
+
     def __init__(self, path: str):
+        """
+        Creates a new `SourcePath` that simply uses the given path
+
+        Parameters
+        ----------
+        :param path: the path
+        """
+
         self._path = path
 
-    def new(env, path: str):
+    def new(env, path):
+        """
+        Creates a new `SourcePath` from given path object
+
+        The path object can be a `SourcePath`, `BuildPath` or `str`. The former two just create a
+        `SourcePath` with the path within the existing object, whereas `str` is interpreted relative
+        to `Env.cur_dir`.
+
+        Parameters
+        ----------
+        :param env: the Environment
+        :param path: the path object
+
+        Returns
+        -------
+        A `SourcePath` object
+        """
+
         if isinstance(path, SourcePath):
             return SourcePath(path._path)
         elif isinstance(path, BuildPath):
@@ -21,10 +56,47 @@ class SourcePath(str):
 
 
 class BuildPath(str):
+    """
+    A path for build files
+
+    This class is used to refer to build files. These are specified relative to the current
+    directory and the build directory in the given environment (`Env.cur_dir`). So, basically build
+    files are put into `"$NPBUILD/" + Env.cur_dir`.
+
+    `BuildPath` implements `__str__` and `__repr__` and can thus be used as a string (the contained
+    path).
+    """
+
     def __init__(self, path: str):
+        """
+        Creates a new `BuildPath` that simply uses the given path
+
+        Parameters
+        ----------
+        :param path: the path
+        """
+
         self._path = path
 
-    def new(env, path: str):
+    def new(env, path):
+        """
+        Creates a new `BuildPath` from given path object
+
+        The path object can be a `SourcePath`, `BuildPath` or `str`. If it's build path, it simply
+        creates a copy. If it's a source path, it produces a `BuildPath` consisting of
+        `Env.build_dir` and the path. If it's a string, it produces a `BuildPath` consisting of
+        `Env.build_dir`, `Env.cur_dir` and the string.
+
+        Parameters
+        ----------
+        :param env: the Environment
+        :param path: the path object
+
+        Returns
+        -------
+        A `BuildPath` object
+        """
+
         if isinstance(path, BuildPath):
             return BuildPath(path._path)
         elif isinstance(path, SourcePath):
@@ -32,7 +104,24 @@ class BuildPath(str):
         else:
             return BuildPath(env.build_dir + '/' + env.cur_dir + '/' + path)
 
-    def with_file_ext(env, path: str, ext: str):
+    def with_file_ext(env, path, ext: str):
+        """
+        Creates a new `BuildPath` from given path object and file extension
+
+        This method is the same as `BuildPath.new`, but replaces the file extension of the given
+        path with `ext`.
+
+        Parameters
+        ----------
+        :param env: the Environment
+        :param path: the path object
+        :param ext: the new file extension
+
+        Returns
+        -------
+        A `BuildPath` object
+        """
+
         (root, cur_ext) = os.path.splitext(path)
         if isinstance(path, BuildPath):
             return BuildPath(root + '.' + ext)
