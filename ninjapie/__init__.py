0134a29dde7f08526d0ad1c20e65de05f69c777c
Nils Asmussen <nils@os.inf.tu-dresden.de>
2023-04-09 20:13:41 +0200

    Bump version 1.0.2 -> 1.0.3.

--- ninjapie/__init__.py
+++ ninjapie/__init__.py
@@ -2,4 +2,4 @@ from ninjapie.env import Env
 from ninjapie.generator import BuildEdge, Generator, Rule
 from ninjapie.path import BuildPath, SourcePath
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
