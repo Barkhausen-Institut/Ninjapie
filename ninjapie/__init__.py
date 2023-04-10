0210906c71e3b787afd28fd0e256a3568fcb0e8d
Nils Asmussen <nils@os.inf.tu-dresden.de>
2023-04-10 08:10:08 +0200

    Added --version argument.
    This is actually a bit tricky, because we want to be able to use
    ninjapie as an installed package and when just having it somewhere in
    our file system (e.g., cloned from Github). Therefore we have a
    version.py with the version number now and import that in
    the __init__.py for packaged usages, but read and exec the version.py
    from the __main__.py for packaged and unpackaged usages.

--- ninjapie/__init__.py
+++ ninjapie/__init__.py
@@ -2,4 +2,4 @@ from ninjapie.env import Env
 from ninjapie.generator import BuildEdge, Generator, Rule
 from ninjapie.path import BuildPath, SourcePath
 
-__version__ = "1.0.3"
+from .version import __version__
