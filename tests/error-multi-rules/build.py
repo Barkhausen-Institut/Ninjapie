42be10bd7e483938cb0fbc3f57c02a78c4b980db
Nils Asmussen <nils@os.inf.tu-dresden.de>
2023-04-11 09:37:30 +0200

    Make multi-error test actually work without the deliberate mistake.

--- tests/error-multi-rules/build.py
+++ tests/error-multi-rules/build.py
@@ -5,12 +5,12 @@ env = Env()
 
 env1 = env.clone()
 env1['CPPFLAGS'] += ['-DTEST=1']
-obj1 = env1.cc(gen, out='hello.o', ins=['hello.c'])
+obj1 = env1.cc(gen, out='foo.o', ins=['foo.c'])
 
 env2 = env.clone()
 env2['CPPFLAGS'] += ['-DTEST=2']
-obj2 = env2.cc(gen, out='hello.o', ins=['hello.c'])
+obj2 = env2.cc(gen, out='foo.o', ins=['foo.c'])
 
-env.c_exe(gen, out='hello', ins=[obj1, obj2])
+env.c_exe(gen, out='hello', ins=[obj1, obj2, 'hello.c'])
 
 gen.write_to_file()
--- tests/error-multi-rules/check.sh
+++ tests/error-multi-rules/check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source "../helper.sh"
-check_error "AssertionError: Output 'build/\\./hello\\.o' is already"
+check_error "AssertionError: Output 'build/\\./foo\\.o' is already"
